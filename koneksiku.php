c1bbe65f2492c70834601c6616367fd4bf172f57
M Rifqi <muhamadrifqi.id@gmail.com>
2024-02-11 19:34:47 +0700

    menambahkan file koneksiku.php dan index.php

---
+++ koneksiku.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$database = "crud1";
+$username = "root";
+$pass = "";
+$conn = new mysqli($servername, $username, $pass, $database);
+
+if (!$conn) {
+    die("connection failed" . mysqli_connect_error());
+}
+echo "Connection Successful";
